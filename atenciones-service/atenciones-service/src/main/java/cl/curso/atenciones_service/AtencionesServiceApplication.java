e65cdfa285a40c8bbcde165d860b0bc7563b2946
Elizabeth-Castillo-E <eliz.castillo@duocuc.cl>
2026-08-26 21:18:01 -0400

    chore: crea los dos proyectos Spring Boot

---
+++ atenciones-service/atenciones-service/src/main/java/cl/curso/atenciones_service/AtencionesServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.curso.atenciones_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtencionesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtencionesServiceApplication.class, args);
+	}
+
+}
---
+++ atenciones-service/atenciones-service/src/test/java/cl/curso/atenciones_service/AtencionesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.curso.atenciones_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtencionesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
