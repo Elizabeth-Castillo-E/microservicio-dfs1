539556c15919ec53407ba81dac65f04e255ffe7a
Elizabeth-Castillo-E <eliz.castillo@duocuc.cl>
2026-08-27 22:04:43 -0400

    asignacion de tipos de roles de usuarios

--- usuarios-service/usuarios-service/src/main/java/cl/curso/usuarios_service/controller/userController.java
+++ usuarios-service/usuarios-service/src/main/java/cl/curso/usuarios_service/controller/userController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/usersRoles")
+@RequestMapping("usersRoles")
 
 public class userController {
     private final userService userService;
--- usuarios-service/usuarios-service/src/main/java/cl/curso/usuarios_service/model/userRole.java
+++ usuarios-service/usuarios-service/src/main/java/cl/curso/usuarios_service/model/userRole.java
@@ -2,48 +2,28 @@ package cl.curso.usuarios_service.model;
 
 public class userRole {
 
-    private final long idUser;
-    private final String userName;
-    private final String userLastName;
-    private final String userPassword;
-    private final String userPhone;
-    private final String userAddress;
-    private final String userMail;
+    private final long idUserRole;
+    private final String userNameRole;
+    private final String userDescriptionRole;
+   
 
-    public userRole(long idUser, String userName, String userLastName, String userPassword, String userPhone, String userAddress, String userMail) {
-        this.idUser = idUser;
-        this.userName = userName;
-        this.userLastName = userLastName;
-        this.userPassword = userPassword;
-        this.userPhone = userPhone;
-        this.userAddress = userAddress;
-        this.userMail = userMail;
+    public userRole(long idUserRole, String userNameRole, String userDescriptionRole) {
+        this.idUserRole = idUserRole;
+        this.userNameRole = userNameRole;
+        this.userDescriptionRole = userDescriptionRole;
     }
 
-    public long getIdUser() {
-        return idUser;
+    public long getIdUserRole() {
+        return idUserRole;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUserNameRole() {
+        return userNameRole;
     }
 
-    public String getUserLastName() {
-        return userLastName;
+    public String getUserDescriptionRole() {
+        return userDescriptionRole;
     }
 
-    public String getUserPassword() {
-        return userPassword;
-    }
 
-    public String getUserPhone() {
-        return userPhone;
-    }
-    public String getUserAddress() {
-        return userAddress;
-    }
-
-    public String getUserMail() {
-        return userMail;
-    }
 }
